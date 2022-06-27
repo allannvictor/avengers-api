7cae164a569bc41dc8bfd5a85b1a25f338dcf070
de Menezes Santos <ardt@gft.com>
2022-06-27 13:30:47 -0300

    first commit

---
+++ src/test/kotlin/projects/allan/avengers/avengers/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package projects.allan.avengers.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
